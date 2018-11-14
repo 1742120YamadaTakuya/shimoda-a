d39405b521f21104b28e481ba9be055e2aa49891
1742120YamadaTakuya <44901122+1742120YamadaTakuya@users.noreply.github.com>
2018-11-15 01:29:35 +0900

    テスト

---
+++ htdocs/search/script.js
@@ -0,0 +1,21 @@
+function readCsv(data) {
+  var target = '#csv-body';
+  var csv = $.csv.toArrays(data);
+  var insert = '';
+  $(csv).each(function() {
+      if (this.length > 0) {
+          insert += '<tr>';
+          $(this).each(function() {
+              insert += '<td>' + this + '</td>';
+          });
+          insert += '</tr>';
+      }
+  });
+  $(target).append(insert);
+}
+
+var csvfile = 'search/database.csv';
+
+$(function(){
+  $.get(csvfile, readCsv, "https");
+});
